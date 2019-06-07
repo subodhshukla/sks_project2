24d8f4dfd6c9f36b7dfeb2079fb08a78d81604c1
subodh <subodh.shukla95@gmail.com>
2019-06-07 16:37:18 +0530

    java file added to repository

---
+++ math.java
@@ -0,0 +1,25 @@
+/**
+* This is test class for GIT
+* Verifing the git functionality
+*/
+//added comment to testt branch2
+public class math{
+	public void add(int a, int b){
+		System.out.println(int c=a+b);
+	}
+	public void sub(int a, int b){
+		System.out.println(int c=a-b);
+	}
+	public void mult(int a, int b){
+		System.out.println(int c=a*b);
+	}
+
+	public static void main(Strings arg[]){
+		math obj=new math();
+		obj.add(8,9);
+		obj.sub(8,9);
+		obj.mult(8,9);
+
+	}
+	//Test for branch1
+}
