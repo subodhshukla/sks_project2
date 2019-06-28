fb221a3c3d50b9e5bfd6c16e017b17c5c950122a
subodh <subodh.shukla95@gmail.com>
2019-06-28 16:09:59 +0530

    changes as added method in test

--- src/test/java/Math.java
+++ src/test/java/Math.java
@@ -1,13 +1,22 @@
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class Math {
- @Test
-    public void calc(){
-     MainClass obj=new MainClass();
-     obj.add(8,9);
-     obj.sub(8,9);
-     obj.mult(8,9);
- }
+    MainClass obj=new MainClass();
 
+    @Test
+    public void add() {
+
+        obj.add(8, 9);
+
+    }
+    @Test
+    public void sub(){
+        obj.sub(8,9);
+    }
+    @Test
+    public void mult(){
+        obj.mult(8,9);
+    }
     //Test for branch1
 }
