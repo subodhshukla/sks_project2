6b29e3b08c4859a7e0711979114ef92781a2bed8
subodh <subodh.shukla95@gmail.com>
2019-06-28 14:51:26 +0530

    changes as added main class in src

---
+++ src/main/java/MainClass.java
@@ -0,0 +1,20 @@
+public class MainClass {
+    int a, b, c;
+    public void add(int a, int b){
+        System.out.println(c=a+b);
+    }
+    public void sub(int a, int b){
+        System.out.println( c=a-b);
+    }
+    public void mult(int a, int b){
+        System.out.println( c=a*b);
+    }
+
+   /* public static void main(String arg[]){
+        MainClass obj=new MainClass();
+        obj.add(8,9);
+        obj.sub(8,9);
+        obj.mult(8,9);
+
+    }*/
+}
--- src/test/java/Math.java
+++ src/test/java/Math.java
@@ -1,21 +1,13 @@
+import org.junit.Test;
+
 public class Math {
-    int a, b, c;
-    public void add(int a, int b){
-        System.out.println(c=a+b);
-    }
-    public void sub(int a, int b){
-        System.out.println( c=a-b);
-    }
-    public void mult(int a, int b){
-        System.out.println( c=a*b);
-    }
+ @Test
+    public void calc(){
+     MainClass obj=new MainClass();
+     obj.add(8,9);
+     obj.sub(8,9);
+     obj.mult(8,9);
+ }
 
-    public static void main(String arg[]){
-        Math obj=new Math();
-        obj.add(8,9);
-        obj.sub(8,9);
-        obj.mult(8,9);
-
-    }
     //Test for branch1
 }
